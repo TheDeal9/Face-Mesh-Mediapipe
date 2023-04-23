e8260be7069b41e4e339f7abb4f79a9e4c6124b7
TheDeal9 <131572925+TheDeal9@users.noreply.github.com>
2023-04-23 20:00:32 +0530

    Update face_mesh_app.py

--- face_mesh_app.py
+++ face_mesh_app.py
@@ -62,7 +62,7 @@ app_mode = st.sidebar.selectbox('Choose the App Mode',
 
 if app_mode == 'About App':
     st.markdown(
-        'In this application we are using **MediaPipe** for creating a Face mash Application. **Streamlit** is an open source app framework in Python language. It helps us create web apps for data science and machine learning in a short time. ')
+        'In this application we are using **MediaPipe** for creating a Face mash Application. **Streamlit** is an open source app framework in Python language. MediaPipe Face Mesh is a face geometry solution that estimates 468 3D face landmarks in real-time even on mobile devices. It employs machine learning (ML) to infer the 3D surface geometry, requiring only a single camera input without the need for a dedicated depth sensor. Utilizing lightweight model architectures together with GPU acceleration throughout the pipeline, the solution delivers real-time performance critical for live experiences. Additionally, the solution is bundled with the Face Geometry module that bridges the gap between the face landmark estimation and useful real-time augmented reality (AR) applications. It establishes a metric 3D space and uses the face landmark screen positions to estimate face geometry within that space. The face geometry data consists of common 3D geometry primitives, including a face pose transformation matrix and a triangular face mesh. Under the hood, a lightweight statistical analysis method called Procrustes Analysis is employed to drive a robust, performant and portable logic. The analysis runs on CPU and has a minimal speed/memory footprint on top of the ML model inference.')
     st.markdown(
         """
         <style>
